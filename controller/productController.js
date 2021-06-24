e824de32d1efc7c060d05b9a9c594fcd578137a4
matiasisaacdavila22 <mipcomputacion@gmail.com>
2021-06-23 21:18:08 -0300

    crud products with model ORM sequelize

--- controller/productController.js
+++ controller/productController.js
@@ -4,18 +4,59 @@ const model = jsonDatabase('productsDataBase');
 const controller = {
 
     index: (req, res) => {
-            let products = model.all();
-            return res.render('product/listProduct', {'products': products});
+        const query = req.query;
+        const productCategory = Number(query.productCategory) ? Number(query.productCategory) : null;
+        const productBrand = Number(query.productBrand) ? Number(query.productBrand) : null;
+        const productName = query.productName ? query.productName: '';
+        db.product.findAll({
+            include: [
+                {
+                    model: Category,
+                    as: 'category',
+                    where: { id: { [ productCategory ? Op.eq : Op.ne ]: productCategory } }
+                },
+                {
+                    model: Brand,
+                    as: 'brand',
+                    where: { id: { [ productBrand ? Op.eq : Op.ne ]: productBrand } }
+                },
+                {
+                    model: products,
+                    as: 'products',
+                    where: {
+                        [Op.or]: [
+                            {first_name: { [Op.substring]: actorNameKeyword }}
+                         ]
+                    }
+                }
+            ]
+        })
+        .then(products => {
+            let respuesta = {
+                meta: {
+                    status : 200,
+                    total: products.length,
+                    url: 'products/'
+                },
+                data: products
+            }
+            return res.render('products/index', { products });
+        })
+        .catch(error => res.send(error));
     },
 
     create: (req, res) => {
          return res.render('product/createProduct');
     },
+
     save: (req, res) => {
      if(req.file){
         let productNew = req.body;  
-           model.create(productNew);
-           res.redirect('/products');
+           Product.create(productNew)
+           .then( confirm => {
+            res.redirect('/products');
+           })
+           .catch(error => res.send(error));
      }else{
         return res.render('product/createProduct');
      }
@@ -23,13 +64,25 @@ const controller = {
     },
 
     detail: (req, res) => {
-        let product = model.find(req.params.id);
-        return res.render('product/detailProduct', {'product':product});
+        db.Product.findByPk(req.params.id,
+            {
+                include : ['category']
+            })
+            .then(product => {
+              return res.render('product/detailProduct', {'product':product});
+            })
+            .catch(error => res.send(error));  
    },
 
     edit: (req, res) => {
-         let product = model.find(req.params.id);
-         return res.render('product/editProduct', {'product':product});
+         db.Product.findByPk(req.params.id,
+            {
+                include : ['category']
+            })
+            .then(product => {
+                return res.render('product/editProduct', {'product':product});
+            })
+            .catch(error => res.send(error)); 
     },
     update: (req, res) => {
         let productUpdate = req.body;
@@ -37,16 +90,21 @@ const controller = {
         if(!productUpdate.file){
             productUpdate.file = model.find(req.params.id).file;
         }
-        console.log(productUpdate);
-        model.update(productUpdate);
-        return res.redirect('/products');
+        Product.update(productUpdate)
+        .then( confirm => {
+            return res.redirect('/products');
+        })
+       .catch( error => res.send(error))
     },
 
     remove: (req, res) => {
         let id = req.params.id;
-        console.log('la ide del `roduct is :'+id)
-        model.delete(id);
+        Product
+        .destroy({where: {id: id}, force: true}) // force: true es para asegurar que se ejecute la acción
+        .then(confirm => {
         return res.redirect('/products');
+    })
+    .catch(error => res.send(error))
     }
 
 };
